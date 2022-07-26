e1827b847915b18c29877f258b40aa00656146d5
Soobratty Anas <soobratty.as@gmail.com>
2022-07-26 15:46:53 +0400

    v-on and v-model

--- app.js
+++ app.js
@@ -2,20 +2,31 @@ const vm = Vue.createApp({
     data () {
         return {
             firstName: "john",
-            lastName: "doe"
+            lastName: "doeswe",
+            url: "https://www.google.com",
+            raw_url : '<a href="https://www.google.com" target="_blank">Google</a>',
+            age: 20
         }
     },
 
     methods: {
         fullName () {
             return `${this.firstName} ${this.lastName.toUpperCase()}`
+        },
+
+        increment () {
+            this.age ++;
+        },
+
+        updateLastName (event) {
+            this.lastName = event.target.value;
         }
     }
 }).mount('#app')
 
-setTimeout(() => {
-    vm.firstName = 'Bob';
-}, 2000)
+// setTimeout(() => {
+//     vm.firstName = 'Bob';
+// }, 2000)
 
 // Vue.createApp({
 //     data () {
